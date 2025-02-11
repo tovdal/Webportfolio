0f45fd4dc1dae449c31deef7d4109ad3e2f6e81c
Tova Dalin <tova.dalin@hotmail.com>
2025-02-11 09:44:32 +0100

    js to remove the thank you from from formspree

--- Script.js
+++ Script.js
@@ -41,3 +41,37 @@ function fetchWeatherData() {
 }
 
 window.addEventListener("load", fetchWeatherData);
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    var form = event.target;
+    var formData = new FormData(form);
+
+    fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        if (response.ok) {
+          form.reset(); // Reset the form
+          document.getElementById("thank-you-message").style.display = "block"; // Show the thank you message
+        } else {
+          response.json().then(function (data) {
+            if (Object.hasOwn(data, "errors")) {
+              alert(data["errors"].map((error) => error["message"]).join(", "));
+            } else {
+              alert("Oops! There was a problem submitting your form");
+            }
+          });
+        }
+      })
+      .catch(function (error) {
+        alert("Oops! There was a problem submitting your form");
+      });
+  });
