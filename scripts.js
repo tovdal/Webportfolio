f2093e2c279dc243df6c16525e394d2da0a8b201
Tova Dalin <tova.dalin@hotmail.com>
2025-03-03 18:23:59 +0100

    Refactor CSS and HTML for improved consistency; update image paths and restore form submission functionality

--- scripts.js
+++ scripts.js
@@ -1,42 +1,3 @@
-/* Weather API */
-/* Removed */
-/* Form submission */
-
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    var form = event.target;
-    var formData = new FormData(form);
-
-    fetch(form.action, {
-      method: form.method,
-      body: formData,
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then(function (response) {
-        if (response.ok) {
-          form.reset(); // Reset the form
-          document.getElementById("contact-form").style.display = "none"; // Hide the form
-          document.getElementById("thank-you-message").style.display = "block"; // Show the thank you message
-        } else {
-          response.json().then(function (data) {
-            if (Object.hasOwn(data, "errors")) {
-              alert(data["errors"].map((error) => error["message"]).join(", "));
-            } else {
-              alert("Oops! Det var ett problem att skicka formuläret");
-            }
-          });
-        }
-      })
-      .catch(function (error) {
-        alert("Oops! Det var ett problem att skicka formuläret");
-      });
-  });
-
 /* Dark mode toggle */
 const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
@@ -58,4 +19,45 @@ themeToggle.addEventListener("click", () => {
 
   // Update button text
   themeToggle.textContent = isDarkMode ? "☀️" : "🌙";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  /* Form submission */
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent the default form submission
+
+      var form = event.target;
+      var formData = new FormData(form);
+
+      fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then(function (response) {
+          if (response.ok) {
+            form.reset(); // Reset the form
+            document.getElementById("contact-form").style.display = "none"; // Hide the form
+            document.getElementById("thank-you-message").style.display =
+              "block"; // Show the thank you message
+          } else {
+            response.json().then(function (data) {
+              if (Object.hasOwn(data, "errors")) {
+                alert(
+                  data["errors"].map((error) => error["message"]).join(", ")
+                );
+              } else {
+                alert("Oops! Det var ett problem att skicka formuläret");
+              }
+            });
+          }
+        })
+        .catch(function (error) {
+          alert("Oops! Det var ett problem att skicka formuläret");
+        });
+    });
 });
